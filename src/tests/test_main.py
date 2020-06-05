5d6eeefb4b13ffac7bbe6f6a45aa31aae7a83511
hailxo <hailxl@gmail.com>
2020-06-05 23:43:46 +0800

    add pytest fixture

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -2,10 +2,9 @@ from starlette.testclient import TestClient
 
 from app.main import app
 
-client = TestClient(app)
 
-
-def test_ping():
-    response = client.get("/ping")
+# 这里 test_app 参数使用 conftest.py 内所提供的同名 fixture
+def test_ping(test_app):
+    response = test_app.get("/ping")
     assert response.status_code == 200
     assert response.json() == {"ping": "pong!"}
