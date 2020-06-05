845d7db5c7fe50aeefe808b9e9b78623337fa639
hailxo <hailxl@gmail.com>
2020-06-05 23:51:01 +0800

    some comments

--- src/app/main.py
+++ src/app/main.py
@@ -6,4 +6,5 @@ app = FastAPI()
 async def pong():
     # some async operation could happen here
     # example: `notes = await get_all_notes()`
+    # see https://fastapi.tiangolo.com/async/ for detail introduce
     return {"ping": "pong!"}
